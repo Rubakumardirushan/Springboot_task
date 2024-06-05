4e5d75d300e410a505de883c30fdb5ad79caad80
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2024-06-05 16:07:45 +0530

    add github

--- demo17/src/main/java/com/examplediru/demo17/Controller/Taskcontroller.java
+++ demo17/src/main/java/com/examplediru/demo17/Controller/Taskcontroller.java
@@ -1,15 +1,17 @@
 package com.examplediru.demo17.Controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 import com.examplediru.demo17.model.Task;
 import com.examplediru.demo17.Repositary.TaskRepositary;
 
 @RestController
+@CrossOrigin
 public class Taskcontroller {
     @Autowired
     private TaskRepositary taskRepositary;
@@ -22,7 +24,13 @@ public class Taskcontroller {
     @PostMapping("api/task")
     public Task Posttask(@RequestBody Task task) {
         taskRepositary.save(task);
+        System.out.println(task);
         return task;
+    }
+
+    @GetMapping("api/task")
+    public List<Task> getTasks() {
+        return taskRepositary.findAll();
     }
 
 }
